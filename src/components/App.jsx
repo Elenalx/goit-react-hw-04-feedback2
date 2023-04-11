a753fbb6e8bfd46cdf56922907e4aeed2f5b2e72
Elena Blagodir <elenalx28@gmail.com>
2023-04-11 22:37:45 +0300

    Переробили app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import  Notiflix  from 'notiflix';
 
 import  Searchbar from './Searchbar/Searchbar';
@@ -13,98 +13,87 @@ import { galleryApi } from 'services/gallery-api';
 
 
 
-export class App extends Component {
-  state = {
-    searchText: '',
-    page: 1,
-    img: [],
-    isLoading: false,
-    buttonTogle: false,
-    data: null,
-    isModal: false,
-    currenPreview: null,
-    totalImage : 0,
-  };
+export default function App() {
+  const [searchText, setSearchText] = useState('');
+  const [page, setPage] = useState(1);
+  const [img, setImg] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [buttonTogle, setButtonTogle] = useState(false);
+  const [isModal, setIsModal] = useState(false);
+  const [currenPreview, setCurrenPreview] = useState('');
+  const [totalImage, setTotalImage] = useState(0);
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.searchText !== this.state.searchText ||
-      this.state.page !== prevState.page
-    ) {
-      this.setState({ isLoading: true});
-
-      galleryApi(this.state.searchText, this.state.page)
-        .then(response => response.json())
-        .then(data => {
-          if (!data.total) {
-            Notiflix.Notify.failure(
-              'Sorry, but nothing was found for your search'
-            );
-          }
-
-          const hits = data.hits;
-          this.buttonTogle(hits.length);
-          
-          
-          this.setState(prevState => ({
-            img: [...prevState.img , ...data.hits ],
-            totalImage:data.total,
-          }));
-
-          console.log(this.state.img);
-        })
-        .catch(error => {
-          console.log(error);
-        })
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
+  useEffect(() => {
+    if (!searchText) {
+      return;
     }
-  }
 
-  openModal = url => {
-    this.setState({ currenPreview: url,
-    isModal:true,
-    });
+    setIsLoading(true);
+
+    galleryApi(searchText, page)
+      .then(response => response.json())
+      .then(data => {
+        if (!data.total) {
+          Notiflix.Notify.failure(
+            'Sorry, but nothing was found for your search'
+          );
+        }
+
+        setImg(prevImg => [...prevImg, ...data.hits]);
+        setTotalImage(data.total);
+
+        const hits = data.hits;
+        buttonToglee(hits.length);
+        console.log(page);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [searchText, page]);
+
+  const openModal = url => {
+    setCurrenPreview(url);
+    setIsModal(true);
   };
 
-modalClose = () => {
-  this.setState({isModal:false})
-}
-
-
-  onLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const modalClose = () => {
+    setIsModal(false);
   };
 
-  buttonTogle = length => {
+  const handleSearch = (searchTextе) => {
+    if (searchText.trim() === searchTextе) {
+  return  Notiflix.Notify.warning("шось нове давай")
+    }
+    setSearchText(searchTextе);
+    setImg([]);
+    setPage(1);
+  };
+
+  const onLoadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  const buttonToglee = length => {
     if (length >= 12) {
-      return this.setState({ buttonTogle: true });
+      return setButtonTogle(true);
     }
-    return this.setState({ buttonTogle: false });
+    return setButtonTogle(false);
   };
 
-  handleSearch = searchText => {
-    this.setState({ searchText, img:[], page:1 });
-  };
+  return (
+    <>
+      <Searchbar handleSearch={handleSearch} />
 
-  render() {
-    const { handleSearch } = this;
-    const { isLoading, buttonTogle,isModal,currenPreview,img,totalImage } = this.state;
-    return (
-      <>
-        <Searchbar handleSearch={handleSearch} />
-       
-        {img.length !== 0 && (<ImageGallery data={img} onImageClick={this.openModal} />)}
-        
-        {isLoading && <Loader />}
-        {img.length !== totalImage && buttonTogle && !isLoading && <Button onClick={this.onLoadMore} />}
-        { isModal && (
-        <Modal onModalClose={this.modalClose}  image={currenPreview}/>
-        )}
-      </>
-    );
-  }
+      {img.length !== 0 && <ImageGallery data={img} onImageClick={openModal} />}
+
+      {isLoading && <Loader />}
+      {img.length !== totalImage && buttonTogle && (
+        <Button onLoadMore={onLoadMore} />
+      )}
+      {isModal && <Modal onModalClose={modalClose} image={currenPreview} />}
+    </>
+  );
 }
